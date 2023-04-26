01db810e6fd921bd9ff1667238ec6119c2883a58
Vee-keme <veliaprecious@gmail.com>
2023-04-27 00:36:05 +0100

    remove favourite feature

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -3,6 +3,7 @@ import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { UserAuth } from "../context/AuthContext";
 import { db } from "../firebase";
 import { updateDoc, doc, onSnapshot } from "firebase/firestore";
+import { AiOutlineClose } from "react-icons/ai";
 
 const SavedShows = () => {
   const [movies, setMovies] = useState([]);
@@ -23,6 +24,19 @@ const SavedShows = () => {
     });
   }, [user?.email]);
 
+  const movieRef = doc(db, "users", `${user?.email}`);
+
+  const deleteShow = async (passedID) => {
+    try {
+      const result = movies.filter((movie) => movie.id !== passedID);
+      await updateDoc(movieRef, {
+        savedShows: result,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">My Shows</h2>
@@ -32,6 +46,7 @@ const SavedShows = () => {
           className=" left-0 bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
           size={40}
         />
+        {console.log(movies)}
         <div
           id={"slider"}
           className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
@@ -49,6 +64,13 @@ const SavedShows = () => {
               <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
                 <p className="whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
                   {movie.title}
+                </p>
+                <p
+                  onClick={() => deleteShow(movie?.id)}
+                  className="absolute text-gray-300 top-4 right-3 "
+                >
+                  {" "}
+                  <AiOutlineClose />{" "}
                 </p>
               </div>
             </div>
